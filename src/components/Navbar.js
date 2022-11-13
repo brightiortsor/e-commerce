2f494825ca50c1fb3e267b9c283de99391223ea8
Bright-Iortsor <iortsor@gmail.com>
2022-11-13 18:18:54 +0100

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import { Search } from "@mui/icons-material";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
@@ -10,21 +11,48 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
 `;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  outline: none;
+  border: none;
+`;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
-const SearchContainer = styled.div``;
+
+const Logo = styled.h2`
+  font-weight: bold;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
 const Navbar = () => {
   return (
     <Container>
@@ -32,13 +60,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            
-            <Search />
-            input
+            <Input />
+            <Search style={{color: "gray", fontSize: 16}}/>
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>Cynks Kollection</Logo>
+        </Center>
+        <Right>
+          <MenuItem>SIGN UP</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
